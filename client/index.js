e67e7a6920b08609f1ebaec689ed676474246578
Anan Hartanto <hartantoaanan@gmail.com>
2020-10-29 15:18:49 +0700

    only register,,, login and logout in other branch

--- client/index.js
+++ client/index.js
@@ -1,16 +1,6 @@
 const server = "http://localhost:3000"
 
-$(document).ready(function (){
-    const token = localStorage.getItem("token")
-    if(token){
-        $("#login").hide()
-        $("#register").hide()
-        read()
-    }else{
-        $("#register").show()
-        $("#login").show()
-    }
-})
+
 function register(event){
     event.preventDefault()
     const email = $("#register-email").val()
@@ -27,28 +17,3 @@ function register(event){
     })
 }
 
-function login(event){
-    event.preventDefault()
-    const email = $("#login-email").val()
-    const password = $("#login-password").val()
-
-    $.ajax({
-        method: "POST",
-        url: server + "/users/login",
-        data: { email, password }
-    }).done(response => {
-        const token = response.token
-        localStorage.setItem("token", token)
-        $("#login").hide()
-        $("#home").show()
-    }).fail(err => {
-        console.log(err)
-    })
-}
-
-function logout(){
-    $("#home").hide()
-    $("#login").show()
-    $("#register").show()
-    localStorage.removeItem("token")
-}
