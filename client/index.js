3ade030bd13fbe55642de27bc47fb12b705429ac
Anan Hartanto <hartantoaanan@gmail.com>
2020-10-29 14:15:34 +0700

    update logout

--- client/index.js
+++ client/index.js
@@ -1,5 +1,16 @@
 const server = "http://localhost:3000"
 
+$(document).ready(function (){
+    const token = localStorage.getItem("token")
+    if(token){
+        $("#login").hide()
+        $("#register").hide()
+        read()
+    }else{
+        $("#register").show()
+        $("#login").show()
+    }
+})
 function register(event){
     event.preventDefault()
     const email = $("#register-email").val()
@@ -14,4 +25,30 @@ function register(event){
     }).fail(err => {
         console.log(err)
     })
+}
+
+function login(event){
+    event.preventDefault()
+    const email = $("#login-email").val()
+    const password = $("#login-password").val()
+
+    $.ajax({
+        method: "POST",
+        url: server + "/users/login",
+        data: { email, password }
+    }).done(response => {
+        const token = response.token
+        localStorage.setItem("token", token)
+        $("#login").hide()
+        $("#home").show()
+    }).fail(err => {
+        console.log(err)
+    })
+}
+
+function logout(){
+    $("#home").hide()
+    $("#login").show()
+    $("#register").show()
+    localStorage.removeItem("token")
 }
